8a3ccac78f7b06a944d69166e471712a581707b1
Seismix <seismix.dev@gmail.com>
2025-09-24 08:30:45 +0200

    fix: remove css linting errors

--- src/components/settings/AdvancedSettings.svelte
+++ src/components/settings/AdvancedSettings.svelte
@@ -28,32 +28,50 @@
 
 <label>
     <span>Previous chapter buttton:</span>
-    <input type="text" class="form-control" bind:value={settings.prevChapterBtn} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.prevChapterBtn} />
 </label>
 
 <label>
     <span>Recap button placement:</span>
-    <input type="text" class="form-control" bind:value={settings.togglePlacement} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.togglePlacement} />
 </label>
 
 <label>
     <span>Settings button placement:</span>
-    <input type="text" class="form-control" bind:value={settings.settingsPlacement} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.settingsPlacement} />
 </label>
 
 <label>
     <span>Chapter content:</span>
-    <input type="text" class="form-control" bind:value={settings.chapterContent} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.chapterContent} />
 </label>
 
 <label>
     <span>Chapter title:</span>
-    <input type="text" class="form-control" bind:value={settings.chapterTitle} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.chapterTitle} />
 </label>
 
 <label>
     <span>Fiction title:</span>
-    <input type="text" class="form-control" bind:value={settings.fictionTitle} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.fictionTitle} />
 </label>
 
 <label>
@@ -68,9 +86,12 @@
 
 <label>
     <span>Button to close settings modal:</span>
-    <input type="text" class="form-control" bind:value={settings.closeButtonSelector} />
+    <input
+        type="text"
+        class="form-control"
+        bind:value={settings.closeButtonSelector} />
 </label>
 
 <style>
-    @import '../../lib/styles/forms.css';
+    @import "../../lib/styles/forms.css";
 </style>
